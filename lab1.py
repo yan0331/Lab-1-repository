50ffd7bc93f4b4b16800a83c630a1cfd5e2de13e
rina <yan28@myseneca.ca>
2024-02-12 20:02:01 -0500

    newly updated lab1.py

--- lab1.py
+++ lab1.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
-birth_year = int(input("Enter your birth year: "))
-current_year = 2024 
+try:
+	birth_year = int(input("Enter your birth year: "))
+	current_year = 2024 
+	age = current_year - birth_year
+	
+	print("Your age is:", + age)
 
-age = current_year - birth_year
-
-print("Your age is:", + age)
+except ValueError:
+	print("Please enter an int")
 
 def helloWorld():
 	print('Hello World')
